1a0db53cf2e98aa08c837afe3a25eb0454cc447e
NixonCE <nixoncg27@gmail.com>
2022-07-22 13:46:16 -0600

    Fisrt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphApiKey:'jw5JqYQL2YpXY8D2AXOYH0TtEaMdAALG'
 };
 
 /*
